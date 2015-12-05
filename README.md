86679f416a5fd55ed1dcc0e55d78299c750495fc
Mike DeVita <mike@relative.media>
2015-12-04 22:38:27 -0700

    added a scroll to top

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 ```bash
 cd /var/www/blog.relative.media/content/themes && \
 wget https://github.com/RelativeMedia/blog.relative.media/raw/master/dist/build-latest.tar.gz && \
-rm -rf -y relativemedia/* && \
+setopt rmstarsilent && \
+rm -rf -y relativemedia/*; \
 tar -zxvf build-latest.tar.gz -C relativemedia/ && \
 rm -rf build-latest.tar.gz &&
 pm2 restart blog.relative.media;
--- assets/js/index.js
+++ assets/js/index.js
@@ -4,26 +4,28 @@
 
 /*globals jQuery, document */
 (function ($) {
-    "use strict";
-
-    $(document).ready(function(){
-
-      //load up highlightjs
-      hljs.initHighlightingOnLoad();
-
-        // On the home page, move the blog icon inside the header
-        // for better relative/absolute positioning.
-
-        //$("#blog-logo").prependTo("#site-head-content");
-        $(window).scroll(function() {
-            if ($(".navbar").offset().top >= 80) {
-                $(".navbar-fixed-top").addClass("top-nav-collapse");
-                $("#logo").attr("src", "/img/logo_dark_165x50.png");
-            } else {
-                $(".navbar-fixed-top").removeClass("top-nav-collapse");
-                $("#logo").attr("src", "/img/logo_light_165x50.png");
-            }
-        });
+  $(document).ready(function(){
+    hljs.initHighlightingOnLoad();
+    $('.scrollup').click(function () {
+      $("html, body").animate({
+        scrollTop: 0
+      }, 600);
+      return false;
     });
+    $(window).scroll(function() {
+      if ($(".navbar").offset().top >= 80) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+        $("#logo").attr("src", "/img/logo_dark_165x50.png");
+      } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $("#logo").attr("src", "/img/logo_light_165x50.png");
+      }
 
+      if ($(this).scrollTop() > 100) {
+        $('.scrollup').fadeIn();
+      } else {
+        $('.scrollup').fadeOut();
+      }
+    });
+  });
 }(jQuery));
